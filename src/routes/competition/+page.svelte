d531b9625e9b62d3bd88228e2d4bf72f933c50cc
Adrian Mansell <arnie@spoonwibble.com>
2025-10-23 14:06:16 +0100

    Add running order rotation to cheat-sheet. Bold all buttons/keys. Minor editorial fixes. Update figure 18 to one including the Randomise first round checkbox.

--- src/routes/competition/+page.svelte
+++ src/routes/competition/+page.svelte
@@ -92,7 +92,7 @@ The rest of this guide comprises some worked examples introducing how to create 
 <h3>Create the Competition</h3>
 <ul>
     <li>On the menu ribbon, click on the trophy icon to bring up the competition menu.</li>
-    <li>Select Create Competition.</li>
+    <li>Select <b>Create Competition</b>.</li>
 </ul>
 <img src="{base}/figure2.png">
 
@@ -105,7 +105,7 @@ The rest of this guide comprises some worked examples introducing how to create 
 <img src="{base}/figure3.png">
 
 <ul>
-    <li>Click the Create button.</li>
+    <li>Click the <b>Create</b> button.</li>
 </ul>
 
 <p>Your new competition is shown:</p>
@@ -114,19 +114,19 @@ The rest of this guide comprises some worked examples introducing how to create 
 <h3>Add a Stage</h3>
 <ul>
     <li>Click on the bar containing the name of your competition to bring up the competition pop-up.</li>
-    <li>Select Add New Stage</li>
+    <li>Select <b>Add New Stage</b></li>
 </ul>
 
 <img src="{base}/figure5.png">
 
 <ul>
     <li>This brings up the stage pop-up.</li>
-    <li>Only the best two-from-three rounds count, so we add the Use top n rule with a value of 2.</li>
+    <li>Only the best two-from-three rounds count, so we add the <b>Use top n</b> rule with a value of 2.</li>
 </ul>
 <img src="{base}/figure6.png">
 
 <ul>
-    <li>Click the Create button.</li>
+    <li>Click the <b>Create</b> button.</li>
 </ul>
 <p>Your competition now has the stage:</p>
  <img src="{base}/figure7.png">
@@ -134,19 +134,19 @@ The rest of this guide comprises some worked examples introducing how to create 
 <h3>Add Three Rounds</h3>
 <ul>
     <li>Click on Stage 1 to bring up the stage options pop-up.</li>
-    <li>Select Add New Round</li>
+    <li>Select <b>Add New Round</b></li>
 </ul>
  <img src="{base}/figure8.png">
 
  <ul>
     <li>This will bring up the New Round pop-up.</li>
     <li>Select the schedule for the round.</li>
-    <li>For this example, we want the best raw-score normalised to 1000.  Select Best Score in the normalisation pull-down, and enter 1000 in the Normalise to box.</li>
+    <li>For this example, we want the best raw-score normalised to 1000.  Select <b>Best Score</b> in the normalisation pull-down, and enter 1000 in the <b>Normalise to</b> box.</li>
  </ul>
  <img src="{base}/figure9.png">
 
 <ul>
-    <li>Click the Create button.</li>
+    <li>Click the <b>Create</b> button.</li>
     <li>Repeat the above to add rounds 2 and 3.</li>
 </ul>
 <p>Your competition now has three rounds:</p>
@@ -160,7 +160,7 @@ The rest of this guide comprises some worked examples introducing how to create 
 <img src="{base}/figure11.png">
 
 <ul>
-    <li>Enter the name or email address of the FCScore user and click Search.</li>
+    <li>Enter the name or email address of the FCScore user and click <b>Search</b>.</li>
 </ul>
 <img src="{base}/figure12.png">
 
@@ -170,15 +170,15 @@ The rest of this guide comprises some worked examples introducing how to create 
 <img src="{base}/figure13.png">
 
 <ul>
-    <li>If your competitor is already an FCScore User, click on them in the search results and then click Add.</li>
-    <li>If your competitor is not an FCScore User, click on Can't find your user? Create a new one here to bring up the new user pop-up.</li>
+    <li>If your competitor is already an FCScore User, click on them in the search results and then click <b>Add</b>.</li>
+    <li>If your competitor is not an FCScore User, click on <b>Can't find your user? Create a new one here</b> to bring up the new user pop-up.</li>
 </ul>
 <img src="{base}/figure14.png">
 
 <ul>
     <li>Enter the competitors name and select their country.</li>
     <li>It is preferable to enter their email address - this will allow them to claim the account as their own (including their flights) in the future.</li>
-    <li>Click Add.</li>
+    <li>Click <b>Add</b>.</li>
     <li>Repeat for all of your competitors.</li>
 </ul>
 
@@ -188,18 +188,18 @@ The rest of this guide comprises some worked examples introducing how to create 
 <h3>Changing the Running Order</h3>
 <ul>
     <li>In the menu ribbon, you will see that Results is highlighted.</li>
-    <li>To change the running order, click instead on Running Order.  To begin, each competitor has the same default running order in each round:</li>
+    <li>To change the running order, click instead on <b>Running Order</b>.  To begin, each competitor has the same default running order in each round:</li>
 </ul>
 <img src="{base}/figure16.png">
 
 <ul>
-    <li>Click on Stage 1 and select Running Order in the pop-up.</li>
+    <li>Click on Stage 1 and select <b>Running Order</b> in the pop-up.</li>
 </ul>
 <img src="{base}/figure17.png">
 
 <ul>
     <li>Since there are ten competitors, we rotate the running order by three competitors each round.</li>
-    <li>Click Apply.</li>
+    <li>Click <b>Apply</b>.</li>
 </ul>
 <img src="{base}/figure18.png">
 
@@ -211,7 +211,7 @@ The rest of this guide comprises some worked examples introducing how to create 
 <h4>Start Round 1</h4>
 <ul>
     <li>Click on Round 1 to bring up the Round pop-up.</li>
-    <li>Select Open.</li>
+    <li>Select <b>Open</b>.</li>
 </ul>
 <img src="{base}/figure20.png">
 
@@ -221,17 +221,17 @@ The rest of this guide comprises some worked examples introducing how to create 
 <h4>Upload a Flight</h4>
  <ul>
     <li>Create and score the flight as you would normally.</li>
-    <li>Before uploading, select the Link to competition after upload checkbox.</li>
+    <li>Before uploading, select the <b>Link to competition after upload</b> checkbox.</li>
  </ul>
  <img src="{base}/figure22.png">
  
  <ul>
-    <li>Click the Select Competition button.  A pop-up appears with the competitions you can upload to.  Click Select on the correct competition:</li>
+    <li>Click the <b>Select Competition</b> button.  A pop-up appears with the competitions you can upload to.  Click <b>Select</b> on the correct competition:</li>
  </ul>
  <img src="{base}/figure23.png">
 
  <ul>
-    <li>You can now select the round and competitor for the flight, and click Upload.</li>
+    <li>You can now select the round and competitor for the flight, and click <b>Upload</b>.</li>
  </ul>
  <img src="{base}/figure24.png">
 
@@ -243,12 +243,12 @@ The rest of this guide comprises some worked examples introducing how to create 
 <h4>Assign an Already Uploaded Flight</h4> 
  <p>If you or a competitor has already uploaded a flight to the database, you can directly add it to the competition for that competitor for a round.</p>
  <ul>
-    <li>Click on the … for competitor and round you want to assign a flight for.  Select Link Flight from DB.</li>
+    <li>Click on the <b>…</b> for competitor and round you want to assign a flight for.  Select <b>Link Flight from DB</b>.</li>
  </ul>
  <img src="{base}/figure26.png">
 
  <ul>
-    <li>A list of flights is shown for the competitor.  Click Link for the desired flight.</li>
+    <li>A list of flights is shown for the competitor.  Click <b>Link</b> for the desired flight.</li>
  </ul>
  <img src="{base}/figure27.png">
 
@@ -259,7 +259,7 @@ The rest of this guide comprises some worked examples introducing how to create 
 <h4>Close the Round</h4>
  <p>Once you have added flights for the competitors, you should close the round.</p>
  <ul>
-    <li>Click on the Round heading and click Close in the pop-up.</li>
+    <li>Click on the Round heading and click <b>Close</b> in the pop-up.</li>
  </ul>
  <img src="{base}/figure28.png">
 
@@ -282,9 +282,9 @@ The rest of this guide comprises some worked examples introducing how to create 
     </ul>
     <li>The top five competitors will then progress to Final stage.</li>
     <ul>
+        <li>The result from the preliminary stage will be included as the first round.</li>
         <li>The Final competitors will fly two rounds of the final schedule.</li>
         <li>Each flown round will be normalised to 1000, based on the best raw-score in the round.</li>
-        <li>The result from the preliminary stage will be included as a third round.</li>
         <li>The best two of the three scores will count to the final result.</li>
     </ul>
  </ul>
@@ -300,9 +300,9 @@ The rest of this guide comprises some worked examples introducing how to create 
 <h3>Create the Preliminary Stage</h3>
  <p>The preliminary stage has the following features:</p>
  <ul>
-    <li>The stage results are normalised to 1000; for this we set the normalisation to Best Score.</li>
-    <li>Only the best two rounds for each competitor shall count to the stage result.  For this we set Use top n to 2.</li>
-    <li>Only the top five pilots will progress at the end of this stage. For this we use the Progress top n feature and set it to 5.</li>
+    <li>The stage results are normalised to 1000; for this we set the normalisation to <b>Best Score</b>.</li>
+    <li>Only the best two rounds for each competitor shall count to the stage result.  For this we set <b>Use top n</b> to 2.</li>
+    <li>Only the top five pilots will progress at the end of this stage. For this we use the <b>Progress top n</b> feature and set it to 5.</li>
  </ul>
 
  <p>The resulting stage configuration is:</p>
@@ -316,8 +316,8 @@ The rest of this guide comprises some worked examples introducing how to create 
  <p>The final stage has the following features:</p>
  <ul>
     <li>It has three rounds; two flown F25 rounds and one round containing the scores from the Preliminary Stage.</li>
-    <li>For the competitors progressing to this stage, the scores from the Preliminary Stage are not accumulated into the result (instead, they are included as the first round). For this, we select Restart scoring from this stage.</li>
-    <li>Only the best two rounds count towards the result, so we use the Use top n feature set to 2.</li>
+    <li>For the competitors progressing to this stage, the scores from the Preliminary Stage are not accumulated into the result (instead, they are included as the first round). For this, we select <b>Restart scoring from this stage</b>.</li>
+    <li>Only the best two rounds count towards the result, so we use the <b>Use top n</b> feature set to 2.</li>
  </ul>
 
  <p>The stage configuration is:</p> 
@@ -325,7 +325,7 @@ The rest of this guide comprises some worked examples introducing how to create 
 
 <h3>Create the Final Rounds</h3>
 <h4>Round 1</h4>
- <p>This round is not a flown round, instead it contains the scores from the Preliminary Stage.  For this, we use the Score from previous stage feature, selecting the Preliminary Stage:</p>
+ <p>This round is not a flown round, instead it contains the scores from the Preliminary Stage.  For this, we use the <b>Score from previous stage</b> feature, selecting the Preliminary Stage:</p>
  <img src="{base}/figure35.png">
 
 <h4>Rounds 2 and 3</h4>
@@ -343,47 +343,47 @@ The rest of this guide comprises some worked examples introducing how to create 
     <li>The competition will have one round of schedule F3A P27.</li>
     <li>Competitors can add themselves to the competition.</li>
     <li>Competitors must upload and link their chosen flight within 24 hours of flying it.  Only flights flown whilst the competition round is open are allowed.</li>
-    <li>Each competitor may upload only flight.</li>
+    <li>Each competitor may upload only one flight and may not subsequently replace that flight.</li>
     <li>The result will be normalised by best score to 1000 points.</li>
  </ul>
 
 <h3>Create the Competition</h3>
  <p>Following the same steps as Example 1, create a new competition.</p>
  <ul>
-    <li>To allow pilots to enter themselves, we select Allow pilots to enter.</li>
-    <li>To allow pilots to add their own flight, we select Allow pilots to link their own flights.</li>
+    <li>To allow pilots to enter themselves, we select <b>Allow pilots to enter</b>.</li>
+    <li>To allow pilots to add their own flight, we select <b>Allow pilots to link their own flights</b>.</li>
  </ul>
  <img src="{base}/figure38.png">
 
 <h3>Add a Stage</h3> 
  <p>Following the same steps as example 1, create a stage.</p>
  <ul>
-    <li>To ensure competitors fly whilst the round is open, select Must boot whilst round is open.</li>
+    <li>To ensure competitors fly whilst the round is open, select <b>Must boot whilst round is open</b>.</li>
     <li>To ensure that the competitors upload whilst the comp is open and with 24 hours of flying, we:</li>
     <ul>
-        <li>Select Must upload whilst round is open</li>
-        <li>Set upload within (hours from boot) to 24</li>
+        <li>Select <b>Must upload whilst round is open</b></li>
+        <li>Set <b>upload within (hours from boot)</b> to 24</li>
     </ul>
-    <li>To ensure that each competitor can only upload one flight, select Prevent flight overwrite.</li>
+    <li>To ensure that each competitor can only upload one flight, select <b>Prevent flight overwrite</b>.</li>
  </ul>
  <img src="{base}/figure39.png">
 
 <h3>Add a Round</h3>
- <p>Following the same steps as Example 1, add a Round.  The only setting to be made here is the normalisation, which we set to Best Score to 1000.</p>
+ <p>Following the same steps as Example 1, add a Round.  The only setting to be made here is the normalisation, which we set to <b>Best Score</b> to 1000.</p>
  <img src="{base}/figure40.png">
 
 <h3>Run the Competition</h3>
- <p>This is as simple as opening the round by clicking on it and selecting Open.</p>
+ <p>This is as simple as opening the round by clicking on it and selecting <b>Open</b>.</p>
  <img src="{base}/figure41.png">
 
- <p>Your competition will appear in the competitions list, and Competitors can now add themselves by clicking the Enter button. </p>
+ <p>Your competition will appear in the competitions list, and Competitors can now add themselves by clicking the <b>Enter</b> button. </p>
  <img src="{base}/figure42.png">
 
  <p>They will then appear in the competition.</p>
  <img src="{base}/figure43.png">
 
- <p>To add a flight, they can link it as they upload it, or click the ellipsis (…) by their name in the round to link an already uploaded flight (see Example 1).</p>
- <p>Once you are ready to finish the competition, simply close the round by clicking it and selecting close.</p>
+ <p>To add a flight, they can link it as they upload it, or click the ellipsis (<b>…</b>) by their name in the round to link an already uploaded flight (see Example 1).</p>
+ <p>Once you are ready to finish the competition, simply close the round by clicking it and selecting <b>Close</b>.</p>
  
 
 <h1>Cheat Sheet</h1>	
@@ -395,15 +395,15 @@ The rest of this guide comprises some worked examples introducing how to create 
     </tr>
 </thead>
   <tr>
-    <td>Allow Pilots to Enter</td>
+    <td><b>Allow Pilots to Enter</b></td>
     <td>Allows pilots to add themselves to the competition.  Otherwise, only CDs can add competitors.</td>
   </tr>
   <tr>
-    <td>Allow Pilots to Link Their Own Flights</td>
+    <td><b>Allow Pilots to Link Their Own Flights</b></td>
     <td>Allows pilots to upload-and-link or link their own flights.  Otherwise, only CDs can link flights.</td>
   </tr>
   <tr>
-    <td>Start Date and End Date</td>
+    <td><b>Start Date</b> and <b>End Date</b></td>
     <td>These are planning dates.  The actual dates and times in the database are calculated from when you open and close rounds.</td>
   </tr>
 
@@ -413,15 +413,15 @@ The rest of this guide comprises some worked examples introducing how to create 
     </tr>
 </thead>
   <tr>
-    <td>Progress Top N</td>
+    <td><b>Progress Top N</b></td>
     <td>Only the best N competitors will be progressed from this stage to the next stage.</td>
   </tr>
   <tr>
-    <td>Use Top N</td>
+    <td><b>Use Top N</b></td>
     <td>For each competitor, their best N round scores count (other scores are “dropped”).</td>
   </tr>
   <tr>
-    <td>Restart Scoring From this Stage</td>
+    <td><b>Restart Scoring From this Stage</b></td>
     <td>For competitors in this stage, all previous stage scores are dropped.  An example is a FINAL stage where, for those making the FINAL, only the scores from the FINAL decide the result.</td>
   </tr>
 
@@ -431,23 +431,23 @@ The rest of this guide comprises some worked examples introducing how to create 
     </tr>
 </thead>
   <tr>
-    <td>Must Boot whilst Round is Open</td>
+    <td><b>Must Boot whilst Round is Open</b></td>
     <td>Flights will only be accepted if their log file start time is from when the round is open.</td>
   </tr>
   <tr>
-    <td>Must upload whilst Round is Open</td>
+    <td><b>Must upload whilst Round is Open</b></td>
     <td>Flights will only be accepted if they are linked when the round is open.</td>
   </tr>
   <tr>
-    <td>Upload within (N hours)</td>
+    <td><b>Upload within (N hours)</b></td>
     <td>Flight will only be accepted if it is uploaded and linked within N hours of the log file start time.</td>
   </tr>
   <tr>
-    <td>Prevent Flight Overwrite</td>
+    <td><b>Prevent Flight Overwrite</b></td>
     <td>Competitors may only link one flight to a round.  If they subsequently try to link another (probably better!) flight, it will be rejected.</td>
   </tr>
   <tr>
-    <td>Score from Previous Stage</td>
+    <td><b>Score from Previous Stage</b></td>
     <td>Takes the scores from a previous stage and uses them as the Raw Scores in this round.</td>
   </tr>
 
@@ -457,15 +457,34 @@ The rest of this guide comprises some worked examples introducing how to create 
     </tr>
 </thead>
   <tr>
-    <td>Raw Score</td>
+    <td><b>Raw Score</b></td>
     <td>No normalisation is applied; the raw flight scores are used.</td>
   </tr>
   <tr>
-    <td>Best Score N</td>
+    <td><b>Best Score N</b></td>
     <td>Normalisation is applied such that the best score is normalised to N.</td>
   </tr>
   <tr>
-    <td>Average Score N</td>
+    <td><b>Average Score N</b></td>
     <td>Normalisation is applied such that a raw score equal to the average of all scores would be normalised to N.</td>
   </tr>
+
+<thead>
+    <tr>
+        <th colspan="2">Running Order Rotation Options</th>
+    </tr>
+</thead>
+  <tr>
+    <td><b>Continue from previous stage</b></td>
+    <td>The first round of this stage will be taken from the last round of the previous stage (with rotation applied).</td>
+  </tr>
+  <tr>
+    <td><b>Randomise first round</b></td>
+    <td>The first round of this stage will have a random running order applied.</td>
+  </tr>
+  <tr>
+    <td><b>Rotate by N</b></td>
+    <td>The running order will be shifted down by N pilots between rounds.</td>
+  </tr>
+
 </table>
